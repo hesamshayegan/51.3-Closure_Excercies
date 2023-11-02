5a7093f4ca896d6bb494108afc3343a486cc3b5f
hesam <hesamshayegan88@gmail.com>
2023-11-02 07:56:52 -0700

    added curried function

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,21 @@
 function curriedAdd(total) {
 
+    if (total === undefined) total = 0;
+
+    function accumulator(num) {
+        if (num === undefined) return total;
+        total += num;
+        return accumulator;
+    }
+
+    return accumulator;
 }
 
-module.exports = { curriedAdd };
+const add = curriedAdd(50);
+
+console.log(add(5)(10)(15)(20)());
+
+// The concept of closure is applied in the curriedAdd function. 
+// Specifically, the inner function accumulator forms a closure over the total
+// variable, allowing it to "remember" and access the total variable even
+// after the curriedAdd function has finished executing.
